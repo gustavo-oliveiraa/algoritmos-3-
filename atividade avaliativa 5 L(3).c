ae070d6341827424a17a98fc343ac4bffce52d30
Gustavo <80643129+gustavo-oliveiraa@users.noreply.github.com>
2021-06-04 02:11:05 -0300

    Add files via upload

---
+++ atividade avaliativa 5 L(3).c
@@ -0,0 +1,32 @@
+/*5. Elabore um algoritmo para calcular N! (fatorial de N) sendo que o valor inteiro de N é
+fornecido pelo usuário. Sabe-se que: N! = 1x2x...x N-1 x N e 0! = 1. Use o enquanto...faça.*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
+#include<locale.h>
+
+int main(){
+	int fatorial=1, valorN, contador=1;
+
+	printf("---------------------------------------Programa para calcular o fatorial.--------------------------------------------\n");
+	
+	printf("Insira um valor para o qual deseja calcular o fatorial: ");
+	scanf("%d", &valorN);
+	fflush(stdin);
+	
+	if(valorN<0){
+		printf("Somente números inteiros positivos.\n");
+	}else if(valorN==0){
+		printf("Fatorial de 0 e: 1\n");
+	}else{
+		while(contador <= valorN){
+		fatorial = fatorial * contador;
+		contador++;
+		}
+		printf("Fatorial calculado: %d\n", fatorial);
+	}
+	
+	system("pause");
+    return 0;
+}
