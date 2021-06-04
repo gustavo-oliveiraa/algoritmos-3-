e3948d4340cd4245ae69ec81defb7323b96fe2bc
Gustavo <80643129+gustavo-oliveiraa@users.noreply.github.com>
2021-06-04 02:30:31 -0300

    Update atividade avaliativa 5 L(3).c

--- atividade avaliativa 5 L(3).c
+++ atividade avaliativa 5 L(3).c
@@ -7,18 +7,19 @@ fornecido pelo usuário. Sabe-se que: N! = 1x2x...x N-1 x N e 0! = 1. Use o enqu
 #include<locale.h>
 
 int main(){
+	setlocale(LC_ALL, "Portuguese");
 	int fatorial=1, valorN, contador=1;
 
 	printf("---------------------------------------Programa para calcular o fatorial.--------------------------------------------\n");
 	
-	printf("Insira um valor para o qual deseja calcular o fatorial: ");
+	printf("Insira o valor que deseja calcular o fatorial: ");
 	scanf("%d", &valorN);
 	fflush(stdin);
 	
 	if(valorN<0){
 		printf("Somente números inteiros positivos.\n");
 	}else if(valorN==0){
-		printf("Fatorial de 0 e: 1\n");
+		printf("Fatorial de 0 é: 1\n");
 	}else{
 		while(contador <= valorN){
 		fatorial = fatorial * contador;
